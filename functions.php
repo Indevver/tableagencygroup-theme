da3c6d8620decf19cf64c16076c2db740ce3b6f7
jrcreative <jeremy@jeremyrosscreative.com>
2020-04-15 17:00:17 -0700

    add fonts

--- functions.php
+++ functions.php
@@ -5,9 +5,7 @@ function tag_enqueue_styles() {
     $parent_style = 'parent-style';
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        [ $parent_style ],
-        wp_get_theme()->get('Version')
-    );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [$parent_style], wp_get_theme()->get('Version'));
+    wp_enqueue_style( 'typekit', 'https://use.typekit.net/ena6hfe.css' );
+
 }

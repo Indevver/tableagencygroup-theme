5399f7e6e0d149becdfd5b1c4129212c338531c0
Brian Ferdinand <bferdinand@MacBook-Pro.local>
2020-04-17 22:06:15 -0700

    banner work

--- functions.php
+++ functions.php
@@ -6,7 +6,18 @@ function tag_enqueue_styles() {
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [$parent_style], wp_get_theme()->get('Version'));
-    wp_enqueue_style( 'global-styles', get_stylesheet_directory_uri() . '/assets/css/min/master.min.css', [$parent_style], wp_get_theme()->get('Version'));
+    wp_enqueue_style( 'global-styles', get_stylesheet_directory_uri() . '/assets/css/min/master.min.css', [$parent_style], '1.0.1');
     wp_enqueue_style( 'typekit', 'https://use.typekit.net/ena6hfe.css' );
 
 }
+
+
+function tag_setup() {
+	// Add support for editor styles.
+    add_theme_support( 'editor-styles' );
+
+	// Enqueue editor styles.
+	add_editor_style( '/assets/css/min/master.min.css' );
+	add_editor_style( 'https://use.typekit.net/ena6hfe.css' );
+}
+add_action( 'after_setup_theme', 'tag_setup' );
